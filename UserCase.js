48c0a99ed291786afe52cd72fdc62d6f6cc784ba
Yi Ting Seah <k.seahyt@gmail.com>
2016-07-04 14:27:21 +0800

    adding ttt

---
+++ UserCase.js
@@ -0,0 +1,31 @@
+
+/*
+
+Use Case
+User:
+Number of players: 2
+Choose: X or O
+Number of Games
+Score count for players
+Design upon hover:
+
+System:
+Initialize game
+
+Store data
+
+
+
+
+1.  Window prints "Let's play a game!"
+2.  Window prompts choose x or o
+    Create 2 variables odd and even, play 1 is odd, player 2 is even.
+3.  Create a funciton if player hovers on box, result will be change
+        background color
+3.1 If box is clicked, the box will change color.
+3.2 After player 1 clicks, change to player 2's turn
+4.   Create a function horizontal rule,
+
+
+
+*/
